5c0c9e41582b11c45f1d8c67a8932c66818ba414
Guedelho <mateus.guedelho@gmail.com>
2022-09-27 16:56:41 -0300

    Exercise loops

--- src/lectures/demo/loops/loops.go
+++ src/lectures/demo/loops/loops.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-
+	sum := 0
+	fmt.Println(sum)
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println(sum)
+	}
+	for sum > 10 {
+		sum -= 5
+		fmt.Println(sum)
+	}
 }
