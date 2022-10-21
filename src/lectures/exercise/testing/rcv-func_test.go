7f9cd897067c4f50639c31bf1022155cf401440c
Guedelho <mateus.guedelho@gmail.com>
2022-10-21 17:59:04 -0300

    testing exercises

--- src/lectures/exercise/testing/rcv-func_test.go
+++ src/lectures/exercise/testing/rcv-func_test.go
@@ -10,3 +10,33 @@
 //
 //--Notes:
 //* Use `go test -v ./exercise/testing` to run these specific tests
+
+package main
+
+import "testing"
+
+func newPlayer() Player {
+	return Player{
+		name:      "test",
+		health:    100,
+		maxHealth: 100,
+		energy:    100,
+		maxEnergy: 100,
+	}
+}
+
+func TestHealth(t *testing.T) {
+	player := newPlayer()
+	updateHealth(&player, 50)
+	if player.health != 50 {
+		t.Fatalf("health %v", player.health)
+	}
+}
+
+func TestEnergy(t *testing.T) {
+	player := newPlayer()
+	updateEnergy(&player, 50)
+	if player.energy != 50 {
+		t.Fatalf("energy %v", player.energy)
+	}
+}
