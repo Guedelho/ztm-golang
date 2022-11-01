95410b5ed2787b423b5c2429de65efa8226f1555
Guedelho <mateus.guedelho@gmail.com>
2022-11-01 17:42:26 -0300

    Literals Exercise

--- src/lectures/exercise/func-literal/func-literal.go
+++ src/lectures/exercise/func-literal/func-literal.go
@@ -18,7 +18,18 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
+
+type LineCallback func(line string)
+
+func lineIterator(lines []string, callback LineCallback) {
+	for i := 0; i < len(lines); i++ {
+		callback(lines[i])
+	}
+}
 
 func main() {
 	lines := []string{
@@ -28,4 +39,31 @@ func main() {
 		"12 spaces,",
 		"and 4 punctuation marks in these lines of text!",
 	}
+
+	letters := 0
+	numbers := 0
+	punctuations := 0
+	spaces := 0
+
+	lineFunc := func(line string) {
+		for _, r := range line {
+			if unicode.IsLetter(r) {
+				letters += 1
+			}
+			if unicode.IsDigit(r) {
+				numbers += 1
+			}
+			if unicode.IsPunct(r) {
+				punctuations += 1
+			}
+			if unicode.IsSpace(r) {
+				spaces += 1
+			}
+		}
+	}
+	lineIterator(lines, lineFunc)
+	fmt.Println(letters)
+	fmt.Println(numbers)
+	fmt.Println(punctuations)
+	fmt.Println(spaces)
 }
