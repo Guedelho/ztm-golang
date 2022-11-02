5ee4d2953fa0ed824ae4d6a73960396d4fd1068f
Guedelho <mateus.guedelho@gmail.com>
2022-11-02 12:24:55 -0300

    Sync Exercise

--- src/lectures/exercise/sync/sync.go
+++ src/lectures/exercise/sync/sync.go
@@ -19,7 +19,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"unicode"
 )
 
-func main() {}
+type Count struct {
+	count int
+	sync.Mutex
+}
+
+func countLetters(word string) int {
+	letters := 0
+	for _, ch := range word {
+		if unicode.IsLetter(ch) {
+			letters += 1
+		}
+	}
+	return letters
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var wg sync.WaitGroup
+	totalLetters := Count{}
+	for scanner.Scan() {
+		text := scanner.Text()
+		words := strings.Split(text, " ")
+		for _, word := range words {
+			wordCopy := word
+			wg.Add(1)
+			go func() {
+				totalLetters.Lock()
+				defer totalLetters.Unlock()
+				defer wg.Done()
+				sum := countLetters(wordCopy)
+				totalLetters.count += sum
+			}()
+		}
+	}
+	wg.Wait()
+	totalLetters.Lock()
+	sum := totalLetters.count
+	totalLetters.Unlock()
+	fmt.Println(sum)
+}
