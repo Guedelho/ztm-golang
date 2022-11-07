45fdc6f4816f27525fc6f31324348dea5c5640ea
Guedelho <mateus.guedelho@gmail.com>
2022-11-07 16:18:08 -0300

    Slices demo

--- src/lectures/demo/slices/slices.go
+++ src/lectures/demo/slices/slices.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
+func printRoute(title string, route []string) {
+	fmt.Println("---", title, "---")
+	for i := 0; i < len(route); i++ {
+		element := route[i]
+		fmt.Println(element)
+	}
+}
 
+func main() {
+	route := []string{"Grocery", "Department Store", "Salon"}
+	printRoute("Route 1", route)
+
+	route = append(route, "Home")
+	printRoute("Route 2", route)
+
+	fmt.Println(route[0], "Visited")
+	fmt.Println(route[1], "Visited")
+
+	route = route[2:]
+	printRoute("Route 3", route)
 }
