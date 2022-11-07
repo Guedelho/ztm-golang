e2ac073fa11aa705212adb01f7545f477cad66f0
Guedelho <mateus.guedelho@gmail.com>
2022-11-07 18:18:26 -0300

    Receive functions demo

--- src/lectures/demo/rcv-func/rcv-func.go
+++ src/lectures/demo/rcv-func/rcv-func.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
+type Space struct {
+	occupied bool
+}
 
+type Parkinglot struct {
+	spaces []Space
+}
+
+func occupySpace(lot *Parkinglot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *Parkinglot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *Parkinglot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
+func main() {
+	lot := Parkinglot{spaces: make([]Space, 10)}
+	fmt.Println(lot)
+	lot.occupySpace(1)
+	occupySpace(&lot, 2)
+	fmt.Println(lot)
+
+	lot.vacateSpace(2)
+	fmt.Println(lot)
 }
