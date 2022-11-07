cc9bedf32221dd0e3ad815e18ad43857ef5d23e0
Guedelho <mateus.guedelho@gmail.com>
2022-11-07 20:05:05 -0300

    Interfaces demo

--- src/lectures/demo/interfaces/interfaces.go
+++ src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
+type Preparer interface {
+	PreparerDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PreparerDish() {
+	fmt.Println("cook chicken")
+}
+
+func (s Salad) PreparerDish() {
+	fmt.Println("cook salad")
+}
+
+func prepareDishes(dishes []Preparer) {
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		dish.PreparerDish()
+	}
+}
+
+func main() {
+	chicken := Chicken("Chicken")
+	salad := Salad("Salad")
+	dishes := []Preparer{chicken, salad}
+	prepareDishes(dishes)
 }
