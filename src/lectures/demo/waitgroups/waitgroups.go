584043d47c82daa86fb1816ec1ff7b8e923b32c0
Guedelho <mateus.guedelho@gmail.com>
2022-11-09 18:58:12 -0300

    waitgroups demo

--- src/lectures/demo/waitgroups/waitgroups.go
+++ src/lectures/demo/waitgroups/waitgroups.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		counter += 1
+		go func() {
+			defer func() {
+				fmt.Println(counter, "remaining")
+				counter -= 1
+				wg.Done()
+			}()
+			duration := time.Duration(rand.Intn(500) * int(time.Millisecond))
+			fmt.Println("waiting", duration)
+			time.Sleep(duration)
+		}()
+		fmt.Println("waiting for goroutines")
+		wg.Wait()
+		fmt.Println("done")
+	}
 }
