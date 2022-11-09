621f2e29a6a4465776fd5f25b7885bc87cb0b675
Guedelho <mateus.guedelho@gmail.com>
2022-11-09 18:34:42 -0300

    Func literals demo

--- src/lectures/demo/func-literal/func-literal.go
+++ src/lectures/demo/func-literal/func-literal.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
 
+func compute(lhs, rhs int, op func(lhs, rhs int) int) int {
+	fmt.Println("Running computation")
+	return op(lhs, rhs)
+}
+
+func main() {
+	fmt.Println("2+2=", compute(2, 2, add))
+	fmt.Println("10-2=", compute(10, 2, func(lhs, rhs int) int {
+		return lhs - rhs
+	}))
+	mul := func(lhs, rhs int) int {
+		fmt.Println("Multiplyting")
+		return lhs * rhs
+	}
+	fmt.Println(compute(3, 3, mul))
 }
